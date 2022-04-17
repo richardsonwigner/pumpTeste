cf007da891184fa653521aba966d1dccce77528a
Richardson <richardm@ucl.br>
2022-04-17 17:50:58 -0300

    s

--- final.py
+++ final.py
@@ -1,27 +1,56 @@
+#!/usr/bin/python
+
+# A simple Python application for controlling a relay board from a Raspberry Pi
+# The application uses the GPIO Zero library (https://gpiozero.readthedocs.io/en/stable/)
+# The relay is connected to one of the Pi's GPIO ports, then is defined as an Output device
+# in GPIO Zero: https://gpiozero.readthedocs.io/en/stable/api_output.html#outputdevice
+
+import sys
 import time
-import RPi.GPIO as GPIO
 
-def relay_on():
-    print("RELAY LIGADO")
-    GPIO.output(GPIO_CONTROL, False)
-def relay_off():
-    print("RELAY DESLIGADO")
-    GPIO.output(GPIO_CONTROL, True)
+# Make sure you install required libraries:
+# https://gpiozero.readthedocs.io/en/stable/installing.html
+import gpiozero
+
+# change this value based on which GPIO port the relay is connected to
+RELAY_PIN = 18
+
+# create a relay obje
+# Triggered by the output pin going low: active_high=False.
+# Initially off: initial_value=False
+relay = gpiozero.OutputDevice(RELAY_PIN, active_high=False, initial_value=False)
 
 
+def set_relay(status):
+    if status:
+        print("Setting relay: ON")
+        relay.on()
+    else:
+        print("Setting relay: OFF")
+        relay.off()
 
-# The main program setup the GPIO
-GPIO_CONTROL = 6
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(GPIO_CONTROL, GPIO.OUT)
 
-# Get input from user to turn on or off relay
-while True:
-    data = input("Input 'on'/'off' to turn on/off relay, or 'exit' to end ")
-    if data == 'on':
-        relay_on()
-    elif data == 'off':
-        relay_off()
-    elif data == 'exit':
-        # program end, clean up GPIO
-        GPIO.cleanup()
+def toggle_relay():
+    print("toggling relay")
+    relay.toggle()
+
+
+def main_loop():
+    # start by turning the relay off
+    set_relay(False)
+    while 1:
+        # then toggle the relay every second until the app closes
+        toggle_relay()
+        # wait a second 
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    try:
+        main_loop()
+    except KeyboardInterrupt:
+        # turn the relay off
+        set_relay(False)
+        print("\nExiting application\n")
+        # exit the application
+        sys.exit(0)
