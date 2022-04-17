e9bc0c342c09de373141c2d8f8a1c202a5a3da22
Richardson <richardm@ucl.br>
2022-04-17 17:18:50 -0300

    codigo

--- final.py
+++ final.py
@@ -1,27 +1,40 @@
-from machine import Pin
 import time
-
-red_led = Pin(14, Pin.OUT)  # RED LED
-green_led = Pin(13, Pin.OUT)  # GREEN LED
-sensor = Pin(27, Pin.IN, Pin.PULL_DOWN)
-pump = Pin(26, Pin.OUT)  # pump
-
-print("LIGANDO")
-
-def pump_on():
-    red_led.value(1)
-    green_led.value(0)
-    pump.value(0)
-    time.sleep(10)
-    print("Pump On - Watering the Plant")
-
-
-def pump_off():
-    red_led.value(0)
-    green_led.value(1)
-    pump.value(1)
-    print("Plant is healthy.")
-
+import RPi.GPIO as GPIO
+import datetime
+import sqlite3
+GPIO.setmode(GPIO.BCM)
+#control relay
+GPIO.setup(18, GPIO.OUT) 
+GPIO.output(18,GPIO.HIGH)
+#Led output
+GPIO.setup(15, GPIO.OUT)
+GPIO.output(15, GPIO.HIGH)  
+#push button
+GPIO.setup(14, GPIO.IN)
+led = False
+print('RODANDO')
 
 while True:
-    pump_on()
+    GPIO.output(18,GPIO.LOW)
+    print("RELAY LIGADO")
+    time.sleep(0.5)
+
+def my_callback(channel):  	#manual watering
+	input_state = GPIO.input(14)
+	if input_state == False:
+		print('Manual watering')
+		GPIO.output(15,GPIO.LOW)	#LED on
+		GPIO.output(18,GPIO.LOW)	#RELAY on
+		time.sleep(0.5)
+	else:
+		GPIO.output(15,GPIO.HIGH)	#LED off
+		GPIO.output(18,GPIO.HIGH)	#RELAY off
+		#write log
+		now = datetime.datetime.now()
+		timeString = now.strftime("%Y-%m-%d %H:%M:%S")
+GPIO.add_event_detect(14, GPIO.RISING, callback=my_callback) 
+try:
+	while True:
+		a=1
+except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
+	GPIO.cleanup()
