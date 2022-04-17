15f2e8a39667b16d2545f21662be6c72d7d6ad0b
Richardson <richardm@ucl.br>
2022-04-17 17:46:39 -0300

    s

--- final.py
+++ final.py
@@ -1,41 +1,32 @@
+import logging
 import time
 import RPi.GPIO as GPIO
-import datetime
-import sqlite3
+
+def relay_on():
+    GPIO.output(GPIO_CONTROL, False)
+
+    logging.basicConfig(format='%(asctime)s %(message)s', filename='/home/pi/GardenBrain/events.log', level=logging.INFO)
+    logging.info('Relay has been manually switched on, from functions.py')
+
+def relay_off():
+    GPIO.output(GPIO_CONTROL, True)
+
+    logging.basicConfig(format='%(asctime)s %(message)s', filename='/home/pi/GardenBrain/events.log', level=logging.INFO)
+    logging.info('Relay has been manually switched off, from functions.py')
+
+
+# The main program setup the GPIO
+GPIO_CONTROL = 6
 GPIO.setmode(GPIO.BCM)
-#control relay
-GPIO.setup(18, GPIO.OUT) 
-GPIO.output(18,GPIO.HIGH)
-#Led output
-GPIO.setup(15, GPIO.OUT)
-GPIO.output(15, GPIO.HIGH)  
-#push button
-GPIO.setup(14, GPIO.IN)
-led = False
+GPIO.setup(GPIO_CONTROL, GPIO.OUT)
 
-GPIO.output(18,GPIO.HIGH)
-
-
+# Get input from user to turn on or off relay
 while True:
-    print("RELAY DESLIGADO")
-    time.sleep(0.5)
-
-def my_callback(channel):  	#manual watering
-	input_state = GPIO.input(14)
-	if input_state == False:
-		print('Manual watering')
-		GPIO.output(15,GPIO.LOW)	#LED on
-		GPIO.output(18,GPIO.LOW)	#RELAY on
-		time.sleep(0.5)
-	else:
-		GPIO.output(15,GPIO.HIGH)	#LED off
-		GPIO.output(18,GPIO.HIGH)	#RELAY off
-		#write log
-		now = datetime.datetime.now()
-		timeString = now.strftime("%Y-%m-%d %H:%M:%S")
-GPIO.add_event_detect(14, GPIO.RISING, callback=my_callback) 
-try:
-	while True:
-		a=1
-except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
-	GPIO.cleanup()
+    data = input("Input 'on'/'off' to turn on/off relay, or 'exit' to end ")
+    if data == 'on':
+        relay_on()
+    elif data == 'off':
+        relay_off()
+    elif data == 'exit':
+        # program end, clean up GPIO
+        GPIO.cleanup()
