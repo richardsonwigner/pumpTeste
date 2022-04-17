3223b8198af59458e7f37cfdf894423d97eb5c39
Richardson <richardm@ucl.br>
2022-04-17 17:58:09 -0300

    s

--- final.py
+++ final.py
@@ -1,13 +1,13 @@
 import RPi.GPIO as GPIO            # import RPi.GPIO module  
 from time import sleep             # lets us have a delay  
-GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD  
-GPIO.setup(14, GPIO.OUT)           # set GPIO14 as an output     
+GPIO.setmode(GPIO.BOARD)             # choose BCM or BOARD  
+GPIO.setup(18, GPIO.OUT)           # set GPIO18 as an output     
 try:  
     while True:
-        GPIO.input(14)          # This line looks unnecessary, what does it do? 
+        GPIO.input(18)          # This line looks unnecessary, what does it do? 
         sleep(0.5)                  # wait half a second
-        GPIO.output(14, 1)  #  set GPIO14 to 1/GPIO.HIGH/True  
-    if GPIO.input(14):         # If GPIO14 is TRUE/HIGH
-        GPIO.output(14, 0)  # Set GPIO to FALSE/LOW
+        GPIO.output(18, 1)  #  set GPIO18 to 1/GPIO.HIGH/True  
+    if GPIO.input(18):         # If GPIO18 is TRUE/HIGH
+        GPIO.output(18, 0)  # Set GPIO to FALSE/LOW
 except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
     GPIO.cleanup()
