cf4ac61508073ae17bcde4f9f086209ea7de1766
Richardson <richardm@ucl.br>
2022-04-25 13:36:15 -0300

    push

--- final.py
+++ final.py
@@ -12,16 +12,6 @@ try:
     while True:
         GPIO.output(18, GPIO.HIGH)
         print('Relay 1 ON')
-        time.sleep(1)
-        GPIO.output(26, GPIO.HIGH)
-        print('Relay 2 ON')
-        time.sleep(1)
-        GPIO.output(18, GPIO.LOW)
-        print('Relay 1 OFF')
-        time.sleep(1)
-        GPIO.output(26, GPIO.LOW)
-        print('Relay 2 OFF')
-        time.sleep(1)
         
 finally:
     GPIO.cleanup()
